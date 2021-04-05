7db07f1c3b32de836adddd1c3d588b91cce69004
aeirya <aeirya@gmail.com>
2021-04-05 20:11:03 +0430

    add fast reader

--- ds/hw1/p4.java
+++ ds/hw1/p4.java
@@ -1,3 +1,6 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -263,20 +266,106 @@ public class p4 {
 
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         NotepadApp app = new NotepadApp();
-        final Scanner sc = new Scanner(System.in);
-        manualTest(sc, app);
-        return;
-        // int n = sc.nextInt();
-        // while (n>0) {
-        //     app.operate(
-        //         sc.next().charAt(0)
-        //     );
-        //     --n;
-        // }
-        // sc.close();
-        // app.print();
+        final Reader sc = new Reader();
+        int n = sc.nextInt();
+        while (n>0) {
+            app.operate(
+                sc.readChar()
+            );
+            --n;
+        }
+        sc.close();
+        app.print();
+    }
+
+
+    static class Reader {
+        private final int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String fileName) throws IOException
+        {
+            din = new DataInputStream(
+                new FileInputStream(fileName));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0;
+            int c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte)c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public Character readChar() throws IOException
+        {
+            return readLine().charAt(0);
+        }
+
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                                BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
     }
 
 }
