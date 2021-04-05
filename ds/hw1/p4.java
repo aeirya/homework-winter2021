e9356aa7b5f308c6dd8c1427731155891347d822
aeirya <aeirya@gmail.com>
2021-04-05 16:07:49 +0430

    p4 in java

--- ds/hw1/p4.java
+++ ds/hw1/p4.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class p4 {
     static interface ILinkedList<T> {
@@ -20,36 +21,53 @@ public class p4 {
                 inserts node after *this
             */
             void insertAfterMe(Node node) {
-                node.next = this.next;
-                this.next = node;
+                // this -> node -> this.next
                 node.previous = this;
+                node.next = this.next;
+                if (this.next != null) 
+                    this.next.previous = next;
+                this.next = node;
             }
 
             boolean isHead() {
-                return previous == null;
+                return previous.isNull();
             }
 
             boolean isTail() {
                 return next == null;
             }
+
+            boolean isNull() {
+                return value == null;
+            }
+
+            @Override
+            public String toString() {
+                if (value == null) return "NULL";
+                return value.toString();
+            }
+
         }
+        
+        private final Node NULL = new Node(null, null, null);
 
         private Node current;
         
         public LinkedList() {
-            current = null;
+            current = NULL;
         }
 
         public void insert(T value) {
-            if (current == null) {
-                current = new Node(value, null, null);   
+            if (current.isNull()) {
+                current = new Node(value, NULL, null);   
+                NULL.insertAfterMe(current);
             } else {
                 current = insert(value, current);
             }
         }
 
         Node insert(T value, Node previous) {
-            var node = new Node(value, previous, previous.next);
+            Node node = new Node(value, previous, previous.next);
             previous.next = node;
             return node;
         }
@@ -57,15 +75,16 @@ public class p4 {
         public void remove() {
             if (! isEmpty()) {
                 if (current.isHead() && current.isTail()) {
-                    current = null;
+                    current = NULL;
                 } else if (current.isTail()) {
                     current.previous.next = null;
                     previous();
                 } else if (current.isHead()) {
-                    current.next.previous = null;
+                    current.next.previous = NULL;
                     next();
                 } else {
-                    current.previous.insertAfterMe(current.next);
+                    current.previous.next = current.next;
+                    current.next.previous = current.previous;
                     previous();
                 }
             }
@@ -84,14 +103,25 @@ public class p4 {
         }
 
         public boolean isEmpty() {
-            return current == null;
+            return current.isNull() && current.next == null;
         }
 
         public T head() {
-            while (! current.isHead()) {
-                previous();
-            }
+            if (isEmpty()) return null;
+            current = NULL.next;
             return current.value;
+        }
+
+        @Override
+        public String toString() {
+            if (!isEmpty()) return "EMPTY";
+            Node value = NULL;
+            StringBuilder str = new StringBuilder();
+            while (value != null) {
+                str.append(value.value);
+                value = value.next;
+            }
+            return str.toString();
         }
     }
 
@@ -108,13 +138,14 @@ public class p4 {
         public void insertChar(char c) {
             data.insert(c);
             ++size;
-            pointerRight();
+            ++index;
         }
 
         public void deleteChar() {
             if (index > 0) {
                 data.remove();
                 --size;
+                --index;
             }
         }
 
@@ -133,11 +164,12 @@ public class p4 {
         }
 
         public void print() {
-            var value = data.head();
+            Character value = data.head();
             while (value != null) {
                 printChar(value);
                 value = data.next();
             }
+            newLine();
         }
 
         void printChar(Character c) {
@@ -149,6 +181,24 @@ public class p4 {
         }
     }
 
+    static class NotepadApp {
+        private Notepad notepad;
+
+        public NotepadApp() {
+            notepad = new Notepad();
+        }
+
+        public void operate(Character c) {
+            if      (c == '<') notepad.pointerLeft();
+            else if (c == '>') notepad.pointerRight();
+            else if (c == '-') notepad.deleteChar();
+            else               notepad.insertChar(c);
+        }
+
+        public void print() {
+            notepad.print();
+        }
+    }
 
     static void test1(Notepad notepad) {
         notepad.insertChar('a');
@@ -160,9 +210,56 @@ public class p4 {
         notepad.deleteChar();
     }
 
+    static void test2(Notepad notepad) {
+        notepad.insertChar('a');
+        notepad.insertChar('b');
+        notepad.insertChar('c');
+        notepad.insertChar('d');
+    }
+
+    static void test3(Notepad notepad) {
+        notepad.insertChar('a');
+        notepad.insertChar('b');
+        notepad.pointerLeft();
+        notepad.insertChar('c');
+        notepad.pointerRight();
+        notepad.insertChar('d');
+    }
+
+    static void test4(Notepad notepad) {
+        notepad.insertChar('a');
+        notepad.insertChar('b');
+        notepad.deleteChar();
+        notepad.insertChar('c');
+    }
+
+    static void test5(Notepad notepad) {
+        notepad.insertChar('b');
+        notepad.pointerLeft();
+        notepad.pointerLeft();
+        notepad.insertChar('o');
+    }
+
+    static void test6(Notepad notepad) {
+        notepad.insertChar('b');
+        notepad.pointerLeft();
+        notepad.deleteChar();
+        notepad.pointerLeft();
+        notepad.insertChar('o');
+    }
+
     public static void main(String[] args) {
-        Notepad notepad = new Notepad();
-        test1(notepad);
-        notepad.print();
+        NotepadApp app = new NotepadApp();
+
+        final Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n>0) {
+            app.operate(
+                sc.next().charAt(0)
+            );
+            --n;
+        }
+        sc.close();
+        app.print();
     }
 }
