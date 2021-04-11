91530c4ef1a209c4a2e99e18139cae04c95e733a
aeirya <aeirya@gmail.com>
2021-04-12 00:28:59 +0430

    add merge sort lib

--- ds/hw2/p2.cc
+++ ds/hw2/p2.cc
@@ -1,12 +1,26 @@
 #include "iostream"
+#include "list"
+#include "array"
+
+using std::array;
+
+void median_of_five(array<int, 5> A, int start_index) {
+    A
+}
+
+int find_median(int A[], int n) {
+     auto x = std::list<int>;
+    //  x.
+}
 
 int main() {
     int n, k;
     std::cin >> n >> k;
 
-    int A[n];
+    std::array<int,n> A;
     for (int i=0; i<n; ++i)
         std::cin >> A[i];
+
 
     
 }
