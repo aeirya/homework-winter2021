f6255b265c03e13d19b7dac4d3ef0145a4a43d3b
aeirya <aeirya@gmail.com>
2021-04-15 02:07:34 +0430

    still memory limit

--- ds/hw2/p2_simple.cc
+++ ds/hw2/p2_simple.cc
@@ -39,9 +39,10 @@ int calc_k(int n, int k) {
 }
 
 /*
-    returns list of list of indices
+    returns list of groups
 */
-vector<vector<int>> categorize(int n, int k) {
+vector<vector<int>> categorize(vector<int>& A, int k) {
+    int n = A.size();
     k = calc_k(n, k);
     vector<vector<int>> groups;
     int j;
@@ -49,7 +50,7 @@ vector<vector<int>> categorize(int n, int k) {
         vector<int> g;
         j = i;
         do {
-            g.push_back(j);
+            g.push_back(A[j]);
             j = (j+k) % n;
         } while (j != i);
         groups.push_back(g);
@@ -81,7 +82,7 @@ void rc() {
 /*
     returns median of a sorted list
 */
-int median(vector<int> sorted) {
+int median(vector<int>& sorted) {
     int n = sorted.size();
     int i = n/2;
     if (2*i != n)
@@ -114,12 +115,11 @@ type equalize_cost(vector<T>& list) {
     return cost;
 }
 
-type solve(vector<int> A, int k) {
-    auto groups = categorize(A.size(), k);;
+type solve(vector<int> &A, int k) {
+    auto groups = categorize(A, k);;
     type cost = 0;
     for (int n=groups.size()-1; n>=0;--n) {
-        auto& group = groups[n];
-        auto list = select(A, group);
+        auto& list = groups[n];
         sort(list.begin(), list.end());
         cost += equalize_cost(list);   
         groups.pop_back();
