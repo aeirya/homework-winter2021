e576e0d637acb338f64846518b2206176e409408
aeirya <aeirya@gmail.com>
2021-04-14 23:06:13 +0430

    aha!

--- ds/hw2/p2_simple.cc
+++ ds/hw2/p2_simple.cc
@@ -4,7 +4,7 @@
 */
 
 #include <iostream>
-// #include <algorithm>
+#include <algorithm>
 #include <vector>
 
 // using std::cout;
@@ -18,10 +18,18 @@ int median() {
     return 0;
 }
 
+template <typename T>
+int inline min(T a, T b) {
+    return a < b ? a : b;
+}
+
 int solve(vector<int> A, int k) {
     for (auto& item : A) {
         std::cout << item;
     }
+    std::cout << "\n";
+
+    k = min(k, (int)A.size()-k);
 
     vector<vector<int>> groups;
     int j;
@@ -30,7 +38,7 @@ int solve(vector<int> A, int k) {
         j = i;
         do {
             g.push_back(j);
-            j = (j+k) % k;
+            j = (j+k) % A.size();
         } while (j != 0);
         groups.push_back(g);
     }
@@ -43,18 +51,18 @@ int solve(vector<int> A, int k) {
     }
 }
 
-
 int main() {
-    std::cout << "HI";
+    std::cout << "HI\n";
     int A[] = {3,2,4,1};
     int n = 4;
+    // std::sort(A, A+4);
     // std::vector<int> vec(A, A+n);
     // std::sort(vec.begin(), vec.end());
 
     // for (int i=0; i<4; ++i)
-    //     std::cout << A[i];
+        // std::cout << A[i];
     
-    // int k;
+    int k;
     solve(vector<int>(A, A+n), 2);
     
 }
