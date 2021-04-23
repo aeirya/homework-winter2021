0f63611dbea110bc8fc522f5e2a2b87733018a27
aeirya <aeirya@gmail.com>
2021-04-24 04:01:26 +0430

    ds p4 update

--- ds/hw2/p4.cc
+++ ds/hw2/p4.cc
@@ -1,3 +1,7 @@
+/*
+    سرباز آتش‌انداز عرب
+*/
+
 #include <iostream>
 #include <algorithm>
 
@@ -6,55 +10,64 @@ using std::cout;
 using std::endl;
 using std::sort;
 
+#define type long long
+
 // int binary_seach(int A[], int n, int x) {
 
 // }
 
-int inline abs(int x) {
+template <typename T>
+T inline abs(T x) {
     return x < 0 ? -x : x;
 }
 
-int inline min(int x, int y) {
+template <typename T>
+T inline min(T x, T y) {
     return x < y ? x : y;
 }
 
-int solve(int n, int m, int a, int b, int S[]) {
-    int time = abs(b-a)-1;
-    int i = min(m-1, time);
+template <typename T>
+T solve(T n, T m, T a, T b, T S[]) {
+    T time = abs(b-a)-1;
+    T i = min(m-1, time);
     sort(S, S+m);
-    int count = 0;
-    while (i >= 0 && time > 0) {
-        if (S[i] < time + a) {
+    T count = 0;
+    T timeToRun = a < b ? a-1 : n-a;
+    while (i >= 0 && time >= 0) {
+        if (S[i] <= time + timeToRun) {
             ++count;
             --time;
         }
         --i;
     }
-
     return count;
 }
 
 int main() {
-    int t;
+    type t;
     cin >> t;
     // array to hold results
-    int A[t];
+    type A[t];
 
-    for (int j=0; j<t; ++j) {
+    for (type j=0; j<t; ++j) {
         // input state
-        int n, m, a, b;
+        type n, m, a, b;
         cin >> n >> m >> a >> b;
 
         // input times
-        int S[m];
-        for (int i=0; i<m; ++i) {
+        type S[m];
+        for (type i=0; i<m; ++i) {
             cin >> S[i];
         }
         // solve
         A[j] = solve(n, m, a, b, S);
     }
 
-    for (int j=0; j<t; ++j) {
+    for (type j=0; j<t; ++j) {
         cout << A[j] << endl;
     }
 }
+
+/*
+    aeirya mohammadi
+*/
