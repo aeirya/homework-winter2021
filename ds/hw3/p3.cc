34881dd7b00aebd0b5a37efab7077f7ba07ee56c
aeirya <aeirya@gmail.com>
2021-05-22 07:12:23 +0430

    p3 with caster

--- ds/hw3/p3.cc
+++ ds/hw3/p3.cc
@@ -4,21 +4,26 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <list>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::list;
 
+#define _int long long
 #define type long long
 
+/*
+    structures
+*/
 struct point {
-    int x, y;
+    _int x, y;
 };
 
 struct interval {
-    int start, end;
+    _int start, end;
 
     bool operator <(interval& other) const { 
         return start < other.start 
@@ -29,31 +34,71 @@ struct interval {
 struct person {
     interval x_interval;
     interval y_interval;
-    int index;
+    _int index;
+};
+
+/*
+    person classes
+*/
+class person_1d {
+    protected:
+    const person* p;
+    virtual interval& get_interval() const;
+
+    public:
+    person_1d(person& p) : p(&p) { }
+    
+    _int start() const
+    { return get_interval().start; }
+
+    _int end() const 
+    { return get_interval().end; }
+
+    bool operator <(person_1d& other) const {
+        return get_interval() < other.get_interval();
+    }
+};
+
+class person_x : person_1d {
+    public:
+    person_x(person& p) : person_1d(p) { }
+
+    virtual interval get_interval() {
+        return p->x_interval;
+    }
+};
+
+class person_y : person_1d {
+    public:
+    person_y(person& p) : person_1d(p) { }
+
+    virtual interval get_interval() {
+        return p->y_interval;
+    }
 };
 
 #pragma region input
 
 point* input_point() {
-    int x,y;
+    _int x,y;
     cin >> x >> y;
     return new point {x-1,y-1};
 }
 
-person* new_person(point& up_left, point& down_right, int index) {
+person* new_person(point& up_left, point& down_right, _int index) {
     interval *x = new interval {up_left.x, down_right.x},
              *y = new interval {up_left.y, down_right.y};
     return new person {*x, *y, index};
 }
 
-vector<person>& input_people(int n) {    
+vector<person>& input_people(_int n) {    
     point
         *up_left,   
         *down_right; 
     person* p;
     vector<person>* people = new vector<person>();
     
-    for (int i=0; i<n; ++i) {
+    for (_int i=0; i<n; ++i) {
         up_left = input_point(),
         down_right = input_point();
         p = new_person(*up_left, *down_right, i);
@@ -64,16 +109,21 @@ vector<person>& input_people(int n) {
 
 #pragma endregion
 
+#pragma region print
 void print(vector<point>& points) {
     if (points.size() == 0) {
         cout << -1 << endl;
         return;
     }
-    // cout << "points size is " << points.size() << endl;
     for (point& p : points) {
         cout << p.x+1 << " " << p.y+1 << endl;
     }
 }
+
+void rc() {
+    cout << "checkpoint!" << endl;
+}
+#pragma endregion print
 
 #pragma region heap impl
 
@@ -172,197 +222,159 @@ class heap {
 #pragma endregion min heap
 
 template <typename T>
-void merge_heaps(heap<T>& to, heap<T>& from) {
-    while (from.size() > 0) {
-        to.add(from.pop());
-    }
+void insert(heap<T>& to, vector<T>& from) {
+    for (auto& item : from) to.add(item);
 }
 
-template <typename T, typename comparator>
-class comparable {
-    private:
-    comparator comp;
-    T value;
-
-    public:
-    comparable(T _value, comparator _comp) : value(_value), comp(_comp) {}
-
-    bool operator <(comparable& other) const {
-        return comp < other;
-    }
-
-    T get_value() { return value; }
-};
-
-class person_1d {
-    private:
-    person* p;
-    interval* intvl;
-
-    public:
-    bool operator <(person_1d& other) const {
-        return *intvl < *other.intvl;
-    }
-
-    interval& get_interval() const { return *intvl; }
-
-    int start() const { return get_interval().start; }
-    int end() const { return get_interval().end; }
-    int& get_index() const { return p->index; }
-
-    person& get_person() const { return *p; }
-
-    person_1d(interval& _intvl, person& _p) : intvl(&_intvl), p(&_p) {}
-    person_1d() : intvl(0), p(0) {}
-};
-
-// using person_1d = comparable<person, interval>;
-
-// template <typename mapper>
-// class comparable_person {
-//     public:
-//     comparable_person(comparable<person>& _comp) : comp(_comp) {}
-
-//     bool operator <(person& other) const {
-//         return comp
-//     }
-
-//     private:
-//     comparable<person> comp;
-// };
-
-// class aug_person : person {
-//     public:
-//     virtual interval& project_interval();
-//     virtual bool operator <(aug_person& other) const;
-// };
-
-void rc() {
-    cout << "checkpoint!" << endl;
+template <typename T>
+void insert(heap<T>& to, list<T>& from) {
+    for (auto& item : from) to.add(item);
 }
 
-template <typename converter>
+template <typename caster>
 /*
-    f: person -> person_1d (x or y)
+    n: # of people
+    m: # of cells
+    cast: person -> person_1d (x or y)
+    has_answer: validity of 'out'
+    out: output result
 */
-vector<int>& solve(int n, int m, vector<person> people, converter f, bool& has_answer) {
-    vector<int>* result = new vector<int>(n, 0);
-    
-    // people requesting for cell[i]
-    heap<person_1d> cells[m];
-    // make list of heaps
+void solve_1d(_int n, _int m, vector<person> people, caster cast, bool& has_answer, vector<_int>& out) {
+    /*
+        generate a list of people for every cell (if there is a request)
+    */
+    list<person_1d> cells[m];
+
+    // make list of list
     person_1d proj;
     for (auto& p : people) {
-        proj = f(p);
-        cells[proj.start()].add(proj);
+        proj = cast(p);
+        cells[proj.start()].push_back(proj);
     }
 
     // as current point proceeds queue gets bigger
     heap<person_1d> queue;
     // start giving cells
-    for (int i=0; i<m; ++i) {
-        merge_heaps(queue, cells[i]);
+    for (_int i=0; i<m; ++i) {
+        insert(queue, cells[i]);
         if (queue.has_item()) {
             auto p = queue.pop();
             if (p.end() < i) {
                 has_answer = false;
                 return *result;
             }
-            (*result)[p.get_index()] = i;
+            out[p.get_index()] = i;
         }
     }
-    return *result;
 }
 
-vector<point>& solve(int n, int m, vector<person>& people, bool& has_answer) {
+void solve(_int n, _int m, vector<person>& people, bool& has_answer, vector<point> out) {
     has_answer = true;
-    auto *v = new vector<point>(n);
-    v->clear();
 
-    vector<int> x = solve(n, m, people, [](person& p){ return person_1d(p.x_interval, p); }, has_answer);
-    if (!has_answer) return *v;
+    vector<_int> x, y;
+    solve_1d(n, m, people, [](person& p){ return person_x(p); }, has_answer, x);
+    if (!has_answer) return;
 
-    vector<int> y = solve(n, m, people, [](person& p){ return person_1d(p.y_interval, p); }, has_answer);
-    if (!has_answer) return *v;
+    solve_1d(n, m, people, [](person& p){ return person_y(p); }, has_answer, y);
+    if (!has_answer) return;
 
-    // cout << "n is " << n << endl;
-    // cout << "x,y size is " << x.size() << "," << y.size() << endl;
-
-    // cout << "points size before: " << v->size() << endl;
-
-    point* p;
-    for (int i=0; i<n; ++i) {
-        p = new point {x[i], y[i]};
-        v->push_back(*p);
+    for (_int i=0; i<n; ++i) {
+        out.push_back(point {x[i], y[i]});
     }
-    // empty the vectors
-    for (int i=0; i<n; ++i) {
-        x.pop_back();
-        y.pop_back();
-    }
-
-    // cout << "points size after: " << v->size() << endl;
-
-    return *v;
-}
-
-int test_heap() {
-    heap<int> h;
-    h.add(10);
-    h.add(9);
-    h.add(11);
-    h.add(20);
-    h.add(30);
-    h.add(40);
-    while (h.size() > 0) {
-        cout << h.pop() << endl;
-    }
-    // h.print();
-    return 0;
-}
-
-int test_input(vector<person>& people) {
-    for (auto& p : people)
-        cout << p.index << " " 
-            << p.x_interval.start << " " << p.x_interval.end << " " 
-            << p.y_interval.start << " " << p.y_interval.end << endl;
-    return 0;
-}
-
-int test_1d_solve(int n, int m, vector<person>& people) {
-    bool has_answer = true;
-    vector<int> x = solve(n, m, people, [](person& p){ return person_1d(p.x_interval, p); }, has_answer);
-    rc();
-    if (!has_answer) cout << -1 << endl;
-    else {
-        for (auto& i : x)
-            cout << i+1 << " ";
-        cout << endl;
-    }
-    return 0;
 }
 
 int main() {
-    // tests
-    // return test_heap();
-    // return test_input(people);
-    // return test_1d_solve(n, m, people);
-
     // input
-    int n, m;
+    _int n, m;
     cin >> n >> m;
     vector<person>& people = input_people(n);
 
     // solve and print
     bool has_answer;
-    auto& answer = solve(n, m, people, has_answer);
+    vector<point> answer;
+    solve(n, m, people, has_answer, answer);
     print(answer);
 
     // free memoery
     delete &answer;
 
-    // note
-    // bool isGreater = [](int a, int b){ return a > b; };
-
     return 0;
 }
+
+//
+// test functions
+//
+
+// int test_heap() {
+//     heap<int> h;
+//     h.add(10);
+//     h.add(9);
+//     h.add(11);
+//     h.add(20);
+//     h.add(30);
+//     h.add(40);
+//     while (h.size() > 0) {
+//         cout << h.pop() << endl;
+//     }
+//     // h.print();
+//     return 0;
+// }
+
+// int test_input(vector<person>& people) {
+//     for (auto& p : people)
+//         cout << p.index << " " 
+//             << p.x_interval.start << " " << p.x_interval.end << " " 
+//             << p.y_interval.start << " " << p.y_interval.end << endl;
+//     return 0;
+// }
+
+// int test_1d_solve(int n, int m, vector<person>& people) {
+//     bool has_answer = true;
+//     vector<int> x = solve(n, m, people, [](person& p){ return person_1d(p.x_interval, p); }, has_answer);
+//     rc();
+//     if (!has_answer) cout << -1 << endl;
+//     else {
+//         for (auto& i : x)
+//             cout << i+1 << " ";
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+///
+/// dump
+///
+
+// class person_1d {
+//     private:
+//     person* p;
+//     interval* intvl;
+
+//     public:
+//     bool operator <(person_1d& other) const {
+//         return *intvl < *other.intvl;
+//     }
+
+//     interval& get_interval() const { return *intvl; }
+
+//     _int start() const { return get_interval().start; }
+//     _int end() const { return get_interval().end; }
+//     _int& get_index() const { return p->index; }
+
+//     person& get_person() const { return *p; }
+
+//     person_1d(interval& _intvl, person& _p) : intvl(&_intvl), p(&_p) {}
+//     person_1d() : intvl(0), p(0) {}
+// };
+
+
+//  [](person& p){ return person_1d(p.x_interval, p); }, has_answer, x);
+
+// note
+// bool isGreater = [](_int a, _int b){ return a > b; };
+
+
+// tests
+// return test_heap();
+// return test_input(people);
+// return test_1d_solve(n, m, people);
