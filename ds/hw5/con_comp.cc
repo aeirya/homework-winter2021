b905539dd7bd00f6320d7ce0a69526ef08f08707
aeirya <aeirya@gmail.com>
2021-07-17 22:56:44 +0430

    backup

---
+++ ds/hw5/con_comp.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <list>
+using std::list;
+
+#include <vector>
+using std::vector;
+
+#include <queue>
+using std::queue;
+
+#include <stack>
+using std::stack;
+
+#include <algorithm>
+using std::sort;
+
+#include "graph.hh"
+
+/*
+    get connected components
+*/
+list<list<int>> get_connected_components(graph& g) {
+    int n = g.size();
+    
+}
+
+int main() {
+
+}
