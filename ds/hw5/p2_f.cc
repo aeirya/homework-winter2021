46a3beef921c339d8b68a24e82b1ac13b2cff0ed
aeirya <aeirya@gmail.com>
2021-07-17 22:28:23 +0430

    upload p2, wong alg

--- ds/hw5/p2_f.cc
+++ ds/hw5/p2_f.cc
@@ -24,7 +24,40 @@ using std::sort;
 
 #define DEBUG 0
 
-#include "graph.hh"
+
+class directed_graph {
+    private: 
+    int n;
+    vector<list<int>> A;
+    
+    public:
+    directed_graph(int _n) {
+        n = _n;
+        A.resize(_n);
+    }
+
+    void add_edge(int from, int to) {
+        A[from].push_back(to);
+    }
+
+    const list<int>& neighbors(int v) {
+        return A[v];
+    }
+
+    directed_graph transpose() {
+        directed_graph g(n);
+        for (int i=0; i<n; ++i) {
+            for (int j : neighbors(i)) {
+                g.add_edge(j, i);
+            }
+        }
+        return g;
+    }
+
+    int get_size() {
+        return n;
+    }
+};
 
 void c() {
     cout << "checkpoint" << endl;
