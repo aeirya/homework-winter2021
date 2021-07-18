6dd19e841dcdb3fe2d77362ab88fee1757a7f8fb
aeirya <aeirya@gmail.com>
2021-07-18 19:11:12 +0430

    disjoint set

--- ds/hw5/p4.cc
+++ ds/hw5/p4.cc
@@ -13,29 +13,70 @@ enum state {
     none, any, at_least_one
 };
 
-class dset {
+class disjoint_set {
     private:
-    vector<int> parents;
-    vector<int> sizes;
+    struct node {
+        node* parent;
+        int size;
+        int value;
+    }; 
+
+    node new_node(int value) {
+        return node {0, 1, value};
+    }
+
+    vector<node> nodes;
 
     public:
-    dset(int n) {
-        parents.resize(n);
+    disjoint_set(int n) {
+        nodes.resize(n);
         for (int i=0; i<n; ++i)
-            parents[i] = -1;
+            nodes[i] = new_node(i);
+    }
+
+    int find(int x) {
+        node *n = &nodes[x];
+        node *p = n->parent;
+
+        while (p) {
+            if (p->parent)
+                n->parent = p->parent;
+            n = p;
+            p = p->parent;
+        }
+        return n->value;
     }
 
     void join(int x, int y) {
-        if (y<=x) {
-            int tmp = x;
-            x = y;
-            y = tmp;
+        int px = find(x),
+            py = find(y);
+        node &nx = nodes[px],
+             &ny = nodes[py];
+        // set smaller the child of bigger
+        if (nx.size <= ny.size) {
+            nx.parent = &ny;
+            ny.size += nx.size;
+        } else {
+            ny.parent = &nx;
+            nx.size += ny.size;
         }
-        parent[x] = y; 
     }
+};
 
+int disjoint_set_test() {
+    disjoint_set ds(5);
+    ds.join(0,1);
+    ds.join(3,4);
+    ds.join(2,3);
+    cout << ds.find(0) << endl;
+    cout << ds.find(3) << endl;
+    ds.join(0, 3);
+    cout << ds.find(2) << endl;
+    return 0;
 }
-int main() {
 
+
+int main() {
+    return disjoint_set_test();
     return 0;
 }
